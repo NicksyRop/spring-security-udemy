7cf752f0c1449ec7568a490328d1ad0975e14f40
Nickson <nnkipkorir@dtbafrica.com>
2024-08-29 12:13:57 +0300

    logic to validate jwt after authentication

--- src/main/java/com/udemy_security/config/filter/JWTTokenValidatorFilter.java
+++ src/main/java/com/udemy_security/config/filter/JWTTokenValidatorFilter.java
@@ -1,14 +1,24 @@
 package com.udemy_security.config.filter;
 
 import com.udemy_security.constants.ApplicationConstants;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.crypto.SecretKey;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author nnkipkorir
@@ -22,7 +32,22 @@ public class JWTTokenValidatorFilter extends OncePerRequestFilter {
         String jwt = request.getHeader(ApplicationConstants.JWT_HEADER);
         if(jwt != null) {
             try{
+                Environment env = getEnvironment(); // able to read all the configs
+                if (env != null) {
+                    //todo: confirm if token is valid then create authentication object
+                    String secret = env.getProperty(ApplicationConstants.JWT_SECRET_KEY, ApplicationConstants.JWT_SECRET_VALUE);//Read secret from configs if not found use default
+                    SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+                    if (secretKey != null) {
+                        Claims payload = Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(jwt).getPayload();
+                        String username = String.valueOf(payload.get("username"));
+                        String authorities = String.valueOf(payload.get("authorities"));
+                        Authentication auth = new UsernamePasswordAuthenticationToken(username, null,
+                                AuthorityUtils.commaSeparatedStringToAuthorityList(authorities));
+                        SecurityContextHolder.getContext().setAuthentication(auth);
 
+                    }
+                }
+                 
 
             }catch (Exception e) {
                 throw new BadCredentialsException("Invalid JWT token");
