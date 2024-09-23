52d009e0366a25b2292ceca8e14892237fd14343
Nickson <nnkipkorir@dtbafrica.com>
2024-09-23 10:33:04 +0300

    get token from own auth server

--- src/main/java/com/udemy_security/config/KeycloakRoleConverter.java
+++ src/main/java/com/udemy_security/config/KeycloakRoleConverter.java
@@ -7,8 +7,6 @@ import org.springframework.security.oauth2.jwt.Jwt;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -22,11 +20,11 @@ public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedA
 
     @Override
     public Collection<GrantedAuthority> convert(Jwt source) {
-        Map<String, Object> realmAccess = (Map<String, Object>) source.getClaims().get("realm_access");
-        if (realmAccess == null || realmAccess.isEmpty()) {
+       ArrayList<String> roles  = (ArrayList<String>) source.getClaims().get("roles"); //todo : replace scope with realm_access for own auth server
+        if (roles == null || roles.isEmpty()) {
             return new ArrayList<>();
         }
-        Collection<GrantedAuthority> returnValue = ((List<String>) realmAccess.get("roles"))
+        Collection<GrantedAuthority> returnValue = roles
                 .stream().map(roleName -> "ROLE_" + roleName)
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
